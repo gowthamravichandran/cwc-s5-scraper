00fafb1ed975e989a3e1beead2cc564d972fe13f
Gowtham Ravichandran <g.ravichandran@draftkings.com>
2024-07-03 00:10:36 -0500

    fix lookup dates

--- src/main.py
+++ src/main.py
@@ -3,13 +3,32 @@ import homepage_scraper
 import os
 import thirailinks
 import datetime
+import re
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
+
+def get_available_episode_dates(directory):
+    episode_dates = []
+    date_pattern = r'\d{4}-\d{2}-\d{2}'
+
+    # Create list of episode dates
+    for filename in os.listdir(directory):
+        match = re.search(date_pattern, filename)
+        if match:
+            date_str = match.group()
+            episode_date = datetime.strptime(date_str, '%Y-%m-%d').date()
+            episode_dates.append(episode_date)
+
+    # Sort the list of dates
+    episode_dates.sort()
+
+    return episode_dates
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
@@ -18,6 +37,7 @@ if __name__ == '__main__':
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
     episodes = homepage_scraper.scrape_cooku_with_comali_s5()
+    available_episode_dates = get_available_episode_dates(output_folder)
     # Print the results
     for episode in episodes:
         print(f"Title: {episode['title']}")
@@ -31,8 +51,8 @@ if __name__ == '__main__':
         print("---")
         print(thirai_links)
         episode_date = episode['date']
-        if episode_date < datetime.date(2024, 6, 15):
-            print(f"Skipping episode {episode_date} before Jun 15")
+        if episode_date < datetime.date(2024, 6, 15) or episode_date in available_episode_dates:
+            print(f"Skipping episode {episode_date}")
             continue
         output_path = output_folder + f"CookuWithComali - {episode_date}.mp4"
         thirailinks.process_thirai_links(thirai_links, output_path)
